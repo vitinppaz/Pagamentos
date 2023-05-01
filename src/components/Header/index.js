51331bba848ebbb662c2d80da695dfe163279984
vitinppaz <pixelpaz2004@gmail.com>
2023-05-01 17:19:46 -0300

    Number, Header Commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import {Box, box, Title} from "../../components";
+
+
+import {Box,  Title} from "../../components";
 
 const Header = ({title}) => {
     return (
-      <Box background ="#006557" fluid height="80px" justify="center"  >
+      <Box background ="#006557" fluid  height="80px" justify="center"  >
         <SafeAreaView 
         style={{
             flexDirection: "row",
@@ -14,6 +16,7 @@ const Header = ({title}) => {
             <Box align="center">
                 <Title>{title}</Title>
             </Box>
+            
         
         </SafeAreaView>
       </Box>
